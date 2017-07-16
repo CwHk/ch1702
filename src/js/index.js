67e292835bec5fdb2ecc7fad7d28cf9bf6e3a208
hukai <704266819@qq.com>
2017-07-16 16:58:12 +0800

    修改

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,18 @@
 $(function(){
-	$('.h_req1 .select_goods').click(function(){   
+	$('.h_req1 .select_goods').click(function(){
 		if($(this).is(':checked')){
-			$(this).find('.select-icon').css("background-image", 'url(../image/单选绿圆_亮.svg)');
+			$(this).next('.select-icon').css("background-image", 'url(../image/单选绿圆_亮.svg)');
 		}else{
-			$(this).find('.select-icon').css("background-image", 'url(../image/单选绿圆_灰.svg)');
+			$(this).next('.select-icon').css("background-image", 'url(../image/单选绿圆_灰.svg)');
 		}
 	});
 	$('.h_req2 .select_goods').click(function(){   
 		if($(this).is(':checked')){
-			$(this).find('.select-icon').css("background-image",'url(../image/单选绿圆_亮.svg)');
+			$(this).next('.select-icon').css("background-image",'url(../image/单选绿圆_亮.svg)');
 			$(this).parents('.only-s').css("border-bottom","1px solid #979797");
 			$('.box-item-right .kinds').css("display","block");
 		}else{
-			$(this).find('.select-icon').css("background-image",'url(../image/单选绿圆_灰.svg)');
+			$(this).next('.select-icon').css("background-image",'url(../image/单选绿圆_灰.svg)');
 			$(this).parents('.only-s').css("border-bottom","none");
 			$('.box-item-right .kinds').css("display","none");
 		}
